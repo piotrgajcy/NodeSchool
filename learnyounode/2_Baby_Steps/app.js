5c508b264af137a97047d0c72b21a70bb8eaab84
Piotr <noschematic@gmail.com>
2017-01-26 13:09:03 +0100

    Solution changed to ES6.

--- learnyounode/2_Baby_Steps/app.js
+++ learnyounode/2_Baby_Steps/app.js
@@ -1,7 +1,7 @@
-var argArray = process.argv;
-var sum = 0;
+const argArray = process.argv;
+let sum = 0;
 
-for (var i = 2; i < argArray.length; i++) {
+for (let i = 2; i < argArray.length; i += 1) {
   sum += Number(argArray[i]);
 }
 
