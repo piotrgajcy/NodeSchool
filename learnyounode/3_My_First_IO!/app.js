f0aecd326771d512a090b0e55302d94272c7a174
Piotr <noschematic@gmail.com>
2017-01-26 13:16:58 +0100

    Solution changed to ES6.

--- learnyounode/3_My_First_IO!/app.js
+++ learnyounode/3_My_First_IO!/app.js
@@ -1,9 +1,8 @@
-var fs = require('fs');
+const fs = require('fs');
 
-var data = fs.readFileSync(process.argv[2]);
-var buf = Buffer.from(data);
-var str = buf.toString();
-var result = str.split('\n').subArray.length - 1;
-
+const data = fs.readFileSync(process.argv[2]);
+const buf = Buffer.from(data);
+const str = buf.toString();
+const result = str.split('\n').length - 1;
 
 console.log(result);
