a6d9c9c0ec319b54f191655cc3060bd857500030
Piotr <noschematic@gmail.com>
2017-01-26 12:55:32 +0100

    Solution changed to ES6.

--- learnyounode/12_HTTP_Uppercaserer/app.js
+++ learnyounode/12_HTTP_Uppercaserer/app.js
@@ -1,11 +1,9 @@
-var http = require('http'),
-  map = require('through2-map'),
-  portNumber = process.argv[2];
+const http = require('http');
+const map = require('through2-map');
 
-var server = http.createServer(function (req, res) {
-  return req.pipe(map(function(chunk) {
-    return chunk.toString().toUpperCase();
-  })).pipe(res);
-});
+const portNumber = process.argv[2];
+
+const server = http.createServer((req, res) => req.pipe(map(chunk =>
+chunk.toString().toUpperCase())).pipe(res));
 
 server.listen(portNumber);
